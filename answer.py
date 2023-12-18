fb943c501d439da6f923afd577276ba73dd0a00b
Akbar Khan <akbar.khan@enine.school>
2023-12-18 14:38:08 +0530

    Committed

--- answer.py
+++ answer.py
@@ -1,2 +1,18 @@
+"""
+    Module Name :- answer
+    Method(s) :- answer
+"""
+
+from solver import solver
+
+
 def answer():
-    return -1
+    """
+    Find the difference of sum of squares of number and squares of sum
+    of numbers between 1-100
+    """
+    return solver(1, 100)
+
+
+if __name__ == "__main__":
+    print(f"answer() = {answer()}")
--- solver.py
+++ solver.py
@@ -1,2 +1,32 @@
-def solver(start, end, even=False, odd=False):
-    return -1
+"""
+    Module Name :- solver
+    Method(s) :- solver
+"""
+
+
+def solver(start, end):
+    """
+    Find the sum of square of numbers and square of sum of numbers
+    over a range
+
+    Args:
+        start(int) :- Starting point of range.
+        end(int) :- Ending point of range.
+
+    Return:
+        Absolute difference of sum of square of numbers and square of sum of
+        numbers.
+    """
+    squares_sum = 0
+    total = 0
+
+    for i in range(start, end + 1):
+        squares_sum += i**2
+        total += i
+
+    diff = (total**2) - squares_sum
+    return abs(diff)
+
+
+if __name__ == "__main__":
+    print(f"solver(1, 10) = {solver(1, 10)}")
