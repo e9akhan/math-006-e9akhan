c556b7f247ffc464d354f76a976b7596b998c7d8
Akbar Khan <akbar.khan@enine.school>
2023-12-18 15:02:59 +0530

    Committed 2

--- solver.py
+++ solver.py
@@ -3,30 +3,28 @@
     Method(s) :- solver
 """
 
-
-def solver(start, end):
+def solver(start, end, even=False, odd=False):
     """
-    Find the sum of square of numbers and square of sum of numbers
-    over a range
+        Find the sum of square of numbers and square of sum of numbers
+        over a range
 
-    Args:
-        start(int) :- Starting point of range.
-        end(int) :- Ending point of range.
+        Args:
+            start(int) :- Starting point of range.
+            end(int) :- Ending point of range.
 
-    Return:
-        Absolute difference of sum of square of numbers and square of sum of
-        numbers.
+        Return:
+            Absolute difference of sum of square of numbers and square of sum of
+            numbers.
     """
     squares_sum = 0
-    total = 0
 
-    for i in range(start, end + 1):
+    for i in range(start, end+1):
         squares_sum += i**2
-        total += i
 
-    diff = (total**2) - squares_sum
+    sums_square = ((end*(end+1)/2)-(start*(start-1)/2))**2
+
+    diff = sums_square - squares_sum
     return abs(diff)
 
-
-if __name__ == "__main__":
-    print(f"solver(1, 10) = {solver(1, 10)}")
+if __name__ == '__main__':
+    print(f'solver(1, 10) = {solver(1, 10)}')
